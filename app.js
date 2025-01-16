7919cde2ecdefe6d8ed5d9de512b134d0463330d
Icesito <113939920+Icesito68@users.noreply.github.com>
2025-01-16 21:09:44 +0000

    idk

--- app.js
+++ app.js
@@ -8,14 +8,18 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
+var bicicletasAPIRouter = require("./routes/api/bicicletas");
+
 // Instantiations
 var app = express();
+
+// Swagger
+var swaggerJsdoc = require("swagger-jsdoc");
+var swaggerUi = require("swagger-ui-express");
 
 // Configurations
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-
-// Middleware
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,9 +28,26 @@ app.use(cookieParser());
 app.use(favicon(path.join(__dirname, "public", "images", "favicon.png")));
 app.use(express.static(path.join(__dirname, "public")));
 
+// Add logs for incoming requests
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
+
+// Add logs for outgoing responses
+app.use((req, res, next) => {
+  const originalSend = res.send;
+  res.send = (data) => {
+    console.log(`Outgoing response: ${res.statusCode} ${res.statusMessage}`);
+    originalSend.call(res, data);
+  };
+  next();
+});
+
 // Routes
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/api/bicicletas", bicicletasAPIRouter);
 
 // error handlers
 app.use(function (req, res, next) {
@@ -43,5 +64,34 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-// Server bootup or server export
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
 module.exports = app;
